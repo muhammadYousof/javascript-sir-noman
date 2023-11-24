efb3e669a33984b754af2bb7956df8c7e79be761
muhammadYousof <my 7370044@gmail.com>
2023-11-24 08:20:26 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,111 @@
+// Variables
+
+var name1 = "Muhammad Yousuf"
+
+var age = "21"
+
+// var Concedinate = name1 + age
+
+document.write(" <br>My Name Is :" + name1)
+document.write(" <br>")
+document.write("<br>My Age Is :" + age + " Years Old")
+
+// chapter 2 prompt
+
+
+var input = prompt("Enter Your Name")
+
+alert(input)
+
+
+// simple calculator
+
+
+var num1 = Number(prompt("write a Number"))
+
+var num2 = Number(prompt("write a Number"))
+
+var operter = prompt("write a Number")
+
+if(operter === "+"){
+    alert(num1 + num2)
+}
+else if(operter === "-"){
+    alert(num1 - num2)
+}
+else if(operter === "*"){
+    alert(num1 * num2)
+}
+else if(operter === "/"){
+    alert(num1 / num2)
+}
+else{
+    alert("Not a Number")
+}
+
+
+        // inlegal Varibal
+
+// var 123user = ""
+// var input user = ""
+// var num-1 = ""
+
+
+            // legel varibal
+
+var user123 = ""
+var inputuser = ""
+var $num = ""
+var Y_A = ""
+
+
+        // major Javascript Tyeps
+
+        //   String Data Tyeps
+
+var user = "Yousuf"
+
+//             // Number Data Tyeps
+
+var input = 12345
+
+//         //    Bollean True / False Data Tyeps
+
+var num1 = "true/false"
+
+
+// Array Operations
+
+
+var bg = ["Red","Green","Blue"]
+console.log(bg)
+
+bg.push("Yellow","Black")
+
+console.log(bg)
+
+bg.pop()
+
+console.log(bg)
+
+alert(bg)
+
+
+
+// Array Splice Method
+
+var y = [1,2,3,4,5,]
+
+console.log(y)
+
+y.splice(3,2,20,25,50)
+
+console.log(y)
+
+alert(y)
+
+
+
+
+
+
